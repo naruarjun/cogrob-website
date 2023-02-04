8fd863ba81a7d6b2496f1ada9a6311939631f6a9
ShrutheeshIR <shrutheesh99@gmail.com>
2023-02-03 18:14:57 -0800

    final test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 
 chmod +x /src/scripts/process.sh
+chmod +x /src/scripts/deploy.sh
 
 cd /src/ && ./scripts/process.sh
 cd /src/ && hugo
+
+cd /src/ && ./scripts/deploy.sh
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,11 +2,10 @@
 
 password="cogrobucsd23!"
 username="cogrob_admin"
-dir_origin="/src/docs/"
+dir_origin="/src/docs/*"
 dir_destination="/home/cogrob_admin/cogrob.org/"
-Ip="cogrob_admin@ps136143.dreamhostps.com"
+Ip="ps136143.dreamhostps.com"
 
-"
 echo "Uploading files to remote server...."
 sshpass -p "$password" rsync -avzh $dir_origin $username@$Ip:$dir_destination
 echo "File upload to remote server completed! ;)"
