327922c5d63e33a5d1448983b5e1cd6842600bbc
ShrutheeshIR <shrutheesh99@gmail.com>
2023-02-03 17:59:14 -0800

    also should not workp2

---
+++ scripts/deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+password="cogrobucsd23!"
+username="cogrob_admin"
+dir_origin="/src/docs/"
+dir_destination="/home/cogrob_admin/cogrob.org/"
+Ip="cogrob_admin@ps136143.dreamhostps.com"
+
+"
+echo "Uploading files to remote server...."
+sshpass -p "$password" rsync -avzh $dir_origin $username@$Ip:$dir_destination
+echo "File upload to remote server completed! ;)"
